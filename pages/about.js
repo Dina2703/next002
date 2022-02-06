1c749d473082540717b4510b536b23797705a8a0
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-06 00:02:03 -0500

    starter Layout and About page starter

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>hello from About page</h1>
+    </div>
+  );
+};
+
+export default About;
