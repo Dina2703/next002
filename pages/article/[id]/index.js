25f5661b80da6e15b5b858fab7570a285e1e061b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-06 02:09:02 -0500

    getServerSideProps()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,30 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-function Article() {
+function article({ article }) {
   // const router = useRouter();
   // const { id } = router.query;
-  return <div>This is an article {id}</div>;
+  console.log(article);
+  return (
+    <>
+      <h1>{article.id}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
 }
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
 
-export default Article;
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export default article;
