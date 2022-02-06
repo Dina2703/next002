1a9898548eb61557ec42210aa38fc22584f3fff2
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-06 00:43:15 -0500

    Custom Document in Next

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,14 @@
 import headerStyles from "../styles/Header.module.css";
 
 export default function Header() {
-  const x = 2;
   return (
     <div>
-      <h1 className="title">
-        <span>News</span>
+      <h1 className={headerStyles.title}>
+        <span>WebDev </span> News
       </h1>
-      <style jsx>
-        {`
-          .title {
-            color: ${x > 3 ? "red" : "green"};
-          }
-        `}
-      </style>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web dev news
+      </p>
     </div>
   );
 }
