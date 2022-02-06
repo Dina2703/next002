fe6ed0df5b200339f16bec9d43a99f961308b953
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-06 01:46:55 -0500

    Nested path

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+function Article() {
+  // const router = useRouter();
+  // const { id } = router.query;
+  return <div>This is an article {id}</div>;
+}
+
+export default Article;
